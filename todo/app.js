904f1f35be3734118f6967ae077e499908e55e41
thinhphungdev <phungthinh0946519356@gmail.com>
2024-09-10 16:42:13 +0700

    feat: keyboard event shorcut for adding new todo item

--- todo/app.js
+++ todo/app.js
@@ -51,3 +51,13 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     LocalStorage.load();
 });
+
+
+// add keyboard shortcuts - to add new todo item (Ctrl + P)
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.key === 'p') {
+        event.preventDefault();
+        const cmd = new Command(Commands.ADD);
+        CommandExecutor.execute(cmd);
+    }
+})
