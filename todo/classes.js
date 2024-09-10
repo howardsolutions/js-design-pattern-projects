2a3e1005c48a203a5a6d0472ff2f993276252d8b
thinhphungdev <phungthinh0946519356@gmail.com>
2024-09-10 14:52:02 +0700

    feat: todo list class method

--- todo/classes.js
+++ todo/classes.js
@@ -36,6 +36,39 @@ class TodoList {
     static getInstance() {
         return this.instance;
     }
+
+    // LIST BEHAVIOUR
+    add(item) {
+        const listArray = Array.from(this.#data);
+        const isItemExist = listArray.filter(t => t.equals(item)).length > 0;
+
+        if (!isItemExist) {
+            this.#data.add(item);
+            // broadcast the changes
+            this.notify();
+        }
+    }
+    
+    delete(todo_text) {
+        const listArray = Array.from(this.#data);
+        const todoToDelete = listArray.filter(t => t.text == todo_text)[0];
+
+        this.#data.delete(todoToDelete);
+        
+        // broadcast the changes
+        this.notify();
+    }
+
+    find(todo_text) {
+        const listArray = Array.from(this.#data);
+        return listArray.find(t => t.text == todo_text);
+    }
+
+    replaceList(list) {
+        this.#data = list;
+        // broadcast the changes
+        this.notify();
+    }
 }
 
 // APPLYING the observer mixin to the prototype of the TodoList class
