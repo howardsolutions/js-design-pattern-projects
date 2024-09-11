10d748c26f9bb884db4c6b8e9d4fa215c1b71699
thinhphungdev <phungthinh0946519356@gmail.com>
2024-09-11 15:31:19 +0700

    feat:  html cart item string interpolation

--- coffee-master/components/CartItem.js
+++ coffee-master/components/CartItem.js
@@ -10,13 +10,15 @@ export default class CartItem extends HTMLElement {
         this.innerHTML = ""; // Clear the element
 
         const template = document.getElementById("cart-item-template");
-        const content = template.content.cloneNode(true);
 
-        this.appendChild(content);    
+        this.innerHTML = interpolate(template.innerHTML,
+            {
+                qty: `${item.quantity}x`,
+                price: `$${item.product.price.toFixed(2)}`,
+                name: item.product.name,
+            }
+        )
 
-        this.querySelector(".qty").textContent = `${item.quantity}x`;
-        this.querySelector(".name").textContent = item.product.name;
-        this.querySelector(".price").textContent = `$${item.product.price.toFixed(2)}`;
         this.querySelector("a.delete-button").addEventListener("click", event => {
             removeFromCart(item.product.id);
         })
